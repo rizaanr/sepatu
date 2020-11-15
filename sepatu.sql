b38e585cc8e6620ed034f3990a30925807f2b715
thmsadi <64199620+thmsadi@users.noreply.github.com>
2020-11-15 12:29:39 -0800

    Add files via upload

---
+++ sepatu.sql
@@ -0,0 +1,281 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 15, 2020 at 02:22 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sepatu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `invoice_id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `tgl_pesan` datetime NOT NULL,
+  `batas_bayar` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `kategori_id` int(11) NOT NULL,
+  `nama_kategori` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`kategori_id`, `nama_kategori`) VALUES
+(1, 'Sneakers'),
+(2, 'Oxfords'),
+(3, 'Anu\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kurir`
+--
+
+CREATE TABLE `kurir` (
+  `kurir_id` int(11) NOT NULL,
+  `nama_kurir` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kurir`
+--
+
+INSERT INTO `kurir` (`kurir_id`, `nama_kurir`) VALUES
+(1, 'JNE'),
+(2, 'JNT'),
+(3, 'Tiki'),
+(4, 'SiCepat'),
+(5, 'Pos Indonesia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `pesanan_id` int(11) NOT NULL,
+  `invoice_id` int(11) NOT NULL,
+  `produk_id` int(11) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `pesanan`
+--
+DELIMITER $$
+CREATE TRIGGER `pesanan_penjualan` AFTER INSERT ON `pesanan` FOR EACH ROW BEGIN
+	UPDATE produk SET stok = stok-NEW.jumlah
+    WHERE produk_id = NEW.produk_id;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `produk_id` int(11) NOT NULL,
+  `nama_produk` varchar(200) NOT NULL,
+  `merk` varchar(100) NOT NULL,
+  `produk_img` text NOT NULL,
+  `deskripsi` text NOT NULL,
+  `harga` int(20) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `kategori_id` int(3) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`produk_id`, `nama_produk`, `merk`, `produk_img`, `deskripsi`, `harga`, `stok`, `kategori_id`, `user_id`) VALUES
+(1, 'CONVERSE KW abang', 'ventela', 'sepatu1.jpg', 'Sepatu', 1000000, 10, 1, 12),
+(2, 'SEPATU APIK NEMEN', 'ventela', 'sepatu2.jpg', 'dfs', 2000000, 4, 2, 12),
+(3, 'RODOK ELEK', 'Salomon', 'sepatu3.jpg', 'asd', 400000, 2, 2, 3),
+(4, 'APIK SIH', 'Sundik', 'sepatu4.jpg', '', 932874, 90, 1, 2),
+(5, 'MAYAN SIH', 'Apa lolo', 'sepatu5.jpg', 'sembarangang', 2000000, 80, 1, 2),
+(6, 'BIASA ', 'AE', 'sepatu6.jpg', '', 300000, 23, 3, 2),
+(7, 'testere', 'asfs', 'sunset-giraffe-rhino-silhouette.jpg', 'sfsd', 34343, 45, 1, 2),
+(10, 'keranjang', 'ask', '1441.png', 'asfsdfsfsdfsdsdjsakfbshjvfsjv jasvfshevfhs fsdhfbehfvehdfv dsjfvsfjsvfje', 989778, 43, 2, 2),
+(11, 'hasjhsd', 'rtertea', 'colorful-52106_960_720.jpg', 'asfdsf', 10000, 32, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL,
+  `name_role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`role_id`, `name_role`) VALUES
+(1, 'Admin'),
+(2, 'Penjual'),
+(3, 'Pembeli');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `user_img` text NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` int(11) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `date_created` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `name`, `user_img`, `email`, `password`, `role`, `is_active`, `date_created`) VALUES
+(10, 'Muhammad Riza', '', 'rizarx3@gmail.com', '$2y$10$zDnO.9XqBFkbGTHEdo4b3e8OVDCNV4gyesahkFIvyB/tSSoaxgZeC', 3, 1, '1605387154'),
+(11, 'tomas', '', 'tomas@gmail.com', '$2y$10$fiXlUOlW4bEtAx4vywm2t.CfVyq07m9n7uK0zsxKUwn7NdQ.EWFqK', 2, 1, '1605387279'),
+(12, 'Thomas Adi S.', '', 'admin@admin.com', '$2y$10$PUoGor8aJGGM92TFQcgnBe6mcfQus9AA2aoxcgmc8TI3aWqYtJzya', 1, 1, '1605387413');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`invoice_id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`kategori_id`);
+
+--
+-- Indexes for table `kurir`
+--
+ALTER TABLE `kurir`
+  ADD PRIMARY KEY (`kurir_id`);
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`pesanan_id`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`produk_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `invoice_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kurir`
+--
+ALTER TABLE `kurir`
+  MODIFY `kurir_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `pesanan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `produk_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
